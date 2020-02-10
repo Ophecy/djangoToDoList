c63684dea48b257309fdf4bbaf6b15d0839cc075
ophecy <arthur.de.berranger@gmail.com>
2020-02-10 23:57:10 +0100

    feat(todolist): add form

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import TodoList
-
+from .forms import TodoForm
 # Create your views here.
 
 
 def index(request):
     todo_list = TodoList.objects.order_by('id')
-    context = {'todo_list': todo_list}
+    form = TodoForm()
+    context = {'todo_list': todo_list, "form": form}
     return render(request, "todolist/index.html", context)
