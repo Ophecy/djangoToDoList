c8489219fed9c5a0187c0dc9ff8105f444ea99a4
ophecy <arthur.de.berranger@gmail.com>
2020-02-10 23:40:10 +0100

    feat(app): display todo list

--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import TodoList
 
 # Create your views here.
 
 
 def index(request):
-    context = {}
+    todo_list = TodoList.objects.order_by('id')
+    context = {'todo_list': todo_list}
     return render(request, "todolist/index.html", context)
